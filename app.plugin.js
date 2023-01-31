fc2511d0b6d05561e2e0da4433dd7b780139284c
Robert Herber <robert@kingstinct.com>
2023-01-31 23:17:29 +0100

    Update app plugin

--- app.plugin.js
+++ app.plugin.js
@@ -1,9 +1,17 @@
 /** @type {import('@expo/config-plugins')} */
-const { withProjectBuildGradle } = require("@expo/config-plugins");
+const {
+  withProjectBuildGradle,
+  withAppBuildGradle,
+  withDangerousMod,
+  withPlugins,
+  createRunOncePlugin,
+} = require("@expo/config-plugins");
+const fs = require("fs");
+const path = require("path");
 
 /** @type {import('@expo/config-plugins').ConfigPlugin} */
-const withMdsLib = (config) =>
-  withProjectBuildGradle(config, (config) => {
+const configureProjectBuildGradle = (cfg) =>
+  withProjectBuildGradle(cfg, (config) => {
     const {
       modResults: { contents },
     } = config;
@@ -11,14 +19,76 @@ const withMdsLib = (config) =>
     const [before, after] = contents.split("allprojects");
 
     // make it work so it replaces repositories under allprojects instead of buildscript
-    const newAfter = after.replace(
-      /repositories\W?{/,
-      'repositories {\n        flatDir { dirs "$rootDir/../node_modules/expo-mds/android/src/main/libs" }\n'
-    );
+    const newAfter = after.includes(
+      'flatDir { dirs "$rootDir/../node_modules/expo-mds/android" }'
+    )
+      ? after
+      : after.replace(
+          /repositories\W?{/,
+          'repositories {\n        flatDir { dirs "$rootDir/../node_modules/expo-mds/android" }\n'
+        );
 
     config.modResults.contents = before + "allprojects" + newAfter;
 
     return config;
   });
 
-module.exports = withMdsLib;
+/** @type {import('@expo/config-plugins').ConfigPlugin} */
+const configureAppBuildGradle = (cfg) =>
+  withAppBuildGradle(cfg, (config) => {
+    const {
+      modResults: { contents },
+    } = config;
+
+    // make it work so it replaces repositories under allprojects instead of buildscript
+    const updated = contents.includes(
+      ` packagingOptions { pickFirst '**/libc++_shared.so' }`
+    )
+      ? contents
+      : contents.replace(
+          /android\W?{/,
+          `android {\npackagingOptions { pickFirst '**/libc++_shared.so' }\n\n`
+        );
+
+    config.modResults.contents = updated;
+
+    return config;
+  });
+
+/** @type {import('@expo/config-plugins').ConfigPlugin} */
+function withPodfile(config) {
+  return withDangerousMod(config, [
+    "ios",
+    async (config) => {
+      const filePath = path.join(
+        config.modRequest.platformProjectRoot,
+        "Podfile"
+      );
+      const contents = fs.readFileSync(filePath, "utf-8");
+
+      const newContents = !contents.includes("movesense")
+        ? contents.replace(
+            /post_install do \|installer\|/,
+            ` pod 'Movesense', :git => 'https://bitbucket.org/movesense/movesense-mobile-lib.git'\n\n post_install do |installer|`
+          )
+        : contents;
+
+      fs.writeFileSync(filePath, newContents, { encoding: "utf-8" });
+
+      return config;
+    },
+  ]);
+}
+
+const pkg = require("./package.json");
+
+/** @type {import('@expo/config-plugins').ConfigPlugin} */
+const mdsPlugins = (config) => {
+  return withPlugins(config, [
+    withPodfile,
+    configureAppBuildGradle,
+    configureProjectBuildGradle,
+  ]);
+};
+
+module.exports = createRunOncePlugin(mdsPlugins, pkg.name, pkg.version);
